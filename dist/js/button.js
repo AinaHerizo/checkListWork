8ae343c59e1d6a707e8b4fce0931a37a895399e8
AinaHerizo <herizoaina09@gmail.com>
2024-12-20 16:58:13 +0300

    add button

---
+++ dist/js/button.js
@@ -0,0 +1,12 @@
+export function codeButton (){
+    let buttonUncheck = document.querySelector("#uncheckAll")
+
+  buttonUncheck.addEventListener("click", ()=>{
+    const invisibleInput = document.querySelectorAll(".check-content input")
+    invisibleInput.forEach(input => {
+        if (input.checked) {
+            input.checked=false
+        }
+    });
+  })
+}
